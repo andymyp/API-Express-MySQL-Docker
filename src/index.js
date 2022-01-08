b30c8ec89c5657a3a3ce73565c5fb8fc5494b798
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2022-01-09 00:57:40 +0700

    finish index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 
+const activityRouter = require("./routes/activity.route");
+const todolistRouter = require("./routes/todolist.route");
+
 dotenv.config();
 
 const app = express();
@@ -15,6 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send({ message: "Server running" });
 });
+
+app.use("/activity-groups", activityRouter.routes);
+app.use("/todo-items", todolistRouter.routes);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server running on port ${SERVER_PORT}`);
