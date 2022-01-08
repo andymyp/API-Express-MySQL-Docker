e0c9ab798afdff095d6dec6c9440283ade641be1
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2022-01-08 20:27:39 +0700

    update database config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,22 @@
-const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-module.exports = mysql.createPool({
-  host: process.env.MYSQL_HOST || "localhost",
-  port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DBNAME || "todo4",
+const HOST = process.env.MYSQL_HOST || "localhost";
+const PORT = process.env.MYSQL_PORT || 3306;
+const USER = process.env.MYSQL_USER || "root";
+const PASSWORD = process.env.MYSQL_PASSWORD || "";
+const DATABASE = process.env.MYSQL_DBNAME || "todo4";
+
+module.exports = new Sequelize(DATABASE, USER, PASSWORD, {
+  host: HOST,
+  dialect: "mysql",
+  operatorsAliases: false,
+  pool: {
+    max: 6,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
